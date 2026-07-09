3fb344a9d92e3de06aceef686b0ed7b7001e1001
TamilarasanG17 <tamilarasang17@gmail.com>
2026-07-09 11:43:51 +0530

    feat:Adding dummy data

---
+++ frontend/fleetdash-frontend/src/data/dashboardData.ts
@@ -0,0 +1,22 @@
+export const dashboardData = [
+  {
+    title: "Total Vehicles",
+    value: 245,
+    color: "text-blue-600",
+  },
+  {
+    title: "Running",
+    value: 198,
+    color: "text-green-600",
+  },
+  {
+    title: "Idle",
+    value: 32,
+    color: "text-yellow-500",
+  },
+  {
+    title: "Alerts",
+    value: 15,
+    color: "text-red-600",
+  },
+];
