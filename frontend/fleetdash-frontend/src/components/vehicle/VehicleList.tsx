39825b286bfc97f3977403983b692061a78e0d82
TamilarasanG17 <tamilarasang17@gmail.com>
2026-07-10 16:33:49 +0530

    feat:Creating the vechiclelist and alertpanel

---
+++ frontend/fleetdash-frontend/src/components/vehicle/VehicleList.tsx
@@ -0,0 +1,33 @@
+import { vehicles } from "../../data/vehicles";
+
+function VehicleList() {
+  return (
+    <div className="bg-white rounded-xl shadow p-5">
+
+      <h2 className="font-bold text-xl mb-4">
+        Vehicles
+      </h2>
+
+      {vehicles.map((vehicle) => (
+        <div
+          key={vehicle.id}
+          className="flex justify-between border-b py-3"
+        >
+          <span>{vehicle.name}</span>
+
+          <span
+            className={
+              vehicle.status === "Running"
+                ? "text-green-600"
+                : "text-yellow-500"
+            }
+          >
+            {vehicle.status}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default VehicleList;
