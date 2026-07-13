c281fec9bd5a692e4b753f5c752e9bc2fcde95a5
TamilarasanG17 <tamilarasang17@gmail.com>
2026-07-13 13:36:14 +0530

    feat:updating the dashboardgrid and creaing the vechiclecontext

--- frontend/fleetdash-frontend/src/pages/Dashboard.tsx
+++ frontend/fleetdash-frontend/src/pages/Dashboard.tsx
@@ -2,13 +2,20 @@ import AlertPanel from "../components/alert/AlertPanel";
 import DashboardGrid from "../components/dashboard/DashboardGrid";
 import LiveMap from "../components/map/LiveMap";
 import VehicleList from "../components/vehicle/VehicleList";
+import useVehicles from "../hooks/useVehicles";
 
 function Dashboard() {
+  const { vehicles } = useVehicles();
+
   return (
     <div>
       <h1 className="mb-6 text-3xl font-bold">
         Fleet Dashboard
       </h1>
+
+      <p className="mb-6">
+        Connected Vehicles: <b>{vehicles.length}</b>
+      </p>
 
       <DashboardGrid />
 
